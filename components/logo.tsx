3428daa19f82145646256d9c5c10f62cea0bc0cb
Maroof Shittu <dbugshe2@gmail.com>
2026-06-22 11:58:08 +0100

    fix: logo component build error

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,11 +1,18 @@
-import { cn } from "@/utils/shared-utils";
-import Image, { ImageProps } from "next/image";
+import { cn } from '@/utils/shared-utils';
+import Image, { ImageProps } from 'next/image';
 
 export const Logo = ({
   className,
   ...props
-}: ImageProps) => {
+}: { className: string } & Omit<ImageProps, 'src' | 'alt'>) => {
   return (
-<Image alt="" width={32} height={32} className={cn("", className)} src="/medbase_light_logo.png"   />
+    <Image
+      alt="mebase africa logo"
+      width={32}
+      height={32}
+      className={cn('', className)}
+      src="/medbase_light_logo.png"
+      {...props}
+    />
   );
 };
